26efb841d28f1b09877be9e9879cd02e5e3ad798
Diego Giraldo <diego.giraldo1030@gmail.com>
2022-11-03 21:16:30 -0500

    Se sube la imagen con la libreria Glide se agregan las lineas de importacion en el archivo bild.gradle.

--- app/src/main/java/com/diegoarc/evaluaciongrupo1/MainActivity.java
+++ app/src/main/java/com/diegoarc/evaluaciongrupo1/MainActivity.java
@@ -3,14 +3,20 @@ package com.diegoarc.evaluaciongrupo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.time.Instant;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView tvTituloPrincipal;
     TextView tvCarlos;
     TextView tvWilliam;
     TextView tvDiego;
-
+    ImageView imgInternet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +25,8 @@ public class MainActivity extends AppCompatActivity {
         tvCarlos = findViewById(R.id.tvCarlos);
         tvWilliam = findViewById(R.id.tvWilliam);
         tvDiego = findViewById(R.id.tvDiego);
+        imgInternet = findViewById(R.id.imgInternet);
+        Glide.with(this).load("https://img.freepik.com/vector-gratis/ilustracion-concepto-programacion_114360-1351.jpg").into(imgInternet);
+
     }
 }
