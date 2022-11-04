b9caa35d06135b002004cc303826a72281c1ce85
Diego Giraldo <diego.giraldo1030@gmail.com>
2022-11-03 19:41:43 -0500

    Se instancian los permisos en el archivo AndroidManifest, se crean los TextView del titulo principal y se crea un layout vertical se ponen TextView con los nombres de los participantes.

--- app/src/main/java/com/diegoarc/evaluaciongrupo1/MainActivity.java
+++ app/src/main/java/com/diegoarc/evaluaciongrupo1/MainActivity.java
@@ -3,12 +3,21 @@ package com.diegoarc.evaluaciongrupo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvTituloPrincipal;
+    TextView tvCarlos;
+    TextView tvWilliam;
+    TextView tvDiego;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTituloPrincipal = findViewById(R.id.tvTituloPrincipal);
+        tvCarlos = findViewById(R.id.tvCarlos);
+        tvWilliam = findViewById(R.id.tvWilliam);
+        tvDiego = findViewById(R.id.tvDiego);
     }
 }
